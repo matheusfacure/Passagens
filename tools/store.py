f926416391f0c0215e6401911317f9804e3b83d3
Matheus Facure Alves <matheusfacure01@gmail.com>
2016-08-22 17:03:40 -0300

    Pega o preço das passagens

--- tools/store.py
+++ tools/store.py
@@ -11,13 +11,30 @@ def get_decolar_text(origem, destino, ida, volta):
 		link += '/1/0/0'
 		wd.get(link)
 		try:
-			text = wd.find_element_by_id('clusters').text # pega o texto da tabela
+			text = wd.find_element_by_id('clusters').text 
 		except:
 			text = None
-
 		return text
+
+
+def process_decolar_line(linha):
+
+	preco = re.findall(r'R\$ ?\d?\d?\.?\d\d\d', linha)[0][3:]
+	preco = int(re.sub(r'\.', '', preco))
+
+	print(preco)
+
+
+def process_decolar_text(decolar_text):
+	if decolar_text == None:
+		return decolar_text
+
+	linhas = decolar_text.split('IDA\n')[1:]
+	dados_decolar = []
+	for linha in linhas:
+		process_decolar_line(linha)
 
 wd = webdriver.Firefox()
 
 texto = get_decolar_text('BSB', 'VCP', '2016-09-08', '2016-10-08')
-print(texto)
+process_decolar_text(texto)
