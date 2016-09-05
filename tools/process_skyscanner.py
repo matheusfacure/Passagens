f415957b1261de781a8089ea7d5bd0eae1b20ddb
Matheus Facure <matheusfacure01@gmail.com>
2016-09-05 18:34:54 -0300

    Processa itinerario de forma a retornar fileiras

--- tools/process_skyscanner.py
+++ tools/process_skyscanner.py
@@ -2,7 +2,11 @@ import simplejson as json
 import datetime as dt
 import time
 import re
+import numpy as np
+import pandas as pd
+
 from pprint import pprint as pp
+
 
 
 def clean_none(data):
@@ -19,32 +23,30 @@ def process_itinerary(dicio):
 	itineraries = dicio['Itineraries']
 	
 	# limpa o texto e retira informações irrelevantes
-	itineraries_list = []
+	rows = []
 	for itinerarie in itineraries:
-		InboundLegId = itinerarie['InboundLegId']
-		OutboundLegId = itinerarie['OutboundLegId']
-		PricingOptions = []
-		
-		for options in itinerarie['PricingOptions']:
-			agents = options['Agents']
-			price = options['Price']
-			QuoteAgeInMinutes = options['QuoteAgeInMinutes']
-			PricingOptions.append([agents, price, QuoteAgeInMinutes])
-		
-		itineraries_list.append([InboundLegId, OutboundLegId, PricingOptions])
-
-	return itineraries_list # InboundLegId, OutboundLegId, agents, price, QAgInMin
+		for option in itinerarie['PricingOptions']:
+			for agent in option['Agents']:
+				row = []
+				row.append(option['Price']) # add preço
+				row.append(option['QuoteAgeInMinutes']) # add QAgeInMin
+				row.append(agent)
+				row.append(itinerarie['InboundLegId'])						 
+				row.append(itinerarie['OutboundLegId'])
+				rows.append(row)
+	return rows
 
 
 
-
-file = 'BSB-VCP-201610191458-201610191458.json'
+file = 'BSB-VCP-201612131212-201612131212.json'
 
 with open(file, 'r') as fp:
 	data = json.load(fp)
 
+print(len(data))
 data = clean_none(data)
+print(len(data))
 
 pdata = process_itinerary(data[0])
-
-pp(pdata[0])
+#pp(data[0])
+pp(pdata)
