d792a9c48bf6dd89523d48675b8f4a59f7e6efa1
Matheus Facure <matheusfacure01@gmail.com>
2016-09-07 22:24:17 -0300

    Processa todo o dicionário json

--- tools/process_skyscanner.py
+++ tools/process_skyscanner.py
@@ -47,6 +47,13 @@ def add_leg_info_to_row(row, leg):
 			row.append(0)
 	
 
+def add_place_info_to_row(dicio, row, row_station):
+	for place in dicio['Places']:
+			if place['Id'] == row[row_station]:
+				row.append(place['Name'])
+				row.append(place['Type'])
+
+
 
 def process(dicio):
 	rows = []
@@ -65,7 +72,6 @@ def process(dicio):
 	# row no formato:
 	# [preço, QAgInMin, Agent, InId, OutId]
 
-
 	# limpa os agentes e adiciona infos relevantes às observações
 	for row in rows:
 		for agent in dicio['Agents']:
@@ -78,8 +84,7 @@ def process(dicio):
 	# [preço, QAgInMin, Agent, InId, OutId,
 	# ag_nome,  ag_optMobile, ag_stat, ag_type]
 
-
-	# limpa os agentes e adiciona infos relevantes às observações
+	# limpa os lags e adiciona infos relevantes às observações
 	for row in rows:
 		for leg in dicio['Legs']:
 
@@ -91,14 +96,38 @@ def process(dicio):
 			if leg['Id'] == row[3]: # se o inId da row e do Lag coincidirem
 				add_leg_info_to_row(row, leg)
 	# row no formato:
-	# [preço, QAgInMin, Agent, InId, OutId,
-	# ag_nome,  ag_optMobile, ag_stat, ag_type
-	# out_saida, out_chegada, out_dura, out_jMode, out_orStat, out_desStat,
-	#	out_stop1, out_stop2, out_stop3, out_OpCarr1, out_OpCarr2, out_OpCarr3,
-	#	out_carr1, out_carr2, out_carr3
+	# [preço(0), QAgInMin(1), Agent(2), InId(3), OutId(4),
+	# ag_nome(5),  ag_optMobile(6), ag_stat(7), ag_type(8)
+	# out_saida(9), out_chegada(10), out_dura(11), out_jMode(12),
+	# 	out_orStat(13), out_desStat(14), out_stop1(15), out_stop2(16),
+	#	out_stop3(17), out_OpCarr1(18), out_OpCarr2(19), out_OpCarr3(20),
+	#	out_carr1(21), out_carr2(22), out_carr3(23)
+	# in_saida(24), in_chegada(25), in_dura(26)_, in_jMode(27), in_orStat(28),
+	#	in_desStat(29), in_stop1(30), in_stop2(31), in_stop3(32),
+	#	in_OpCarr1(33), in_OpCarr2(34), in_OpCarr3(35), in_carr1(36),
+	#	in_carr2(37), in_carr3(38)]
 
+	# limpa os places e adiciona infos relevantes às observações
+	for row in rows:
+		add_place_info_to_row(dicio, row, 13)
+		add_place_info_to_row(dicio, row, 14)
+		add_place_info_to_row(dicio, row, 28)
+		add_place_info_to_row(dicio, row, 29)
+	# row no formato:
+	# row + [ida_or_nome(39), ida_or_tipo(40), ida_dest_nome(41),
+	#	ida_dest_tipo(42)
+	# volta_or_nome(42), volta_or_tipo(44), volta_dest_nome(45),
+	#	volta_dest_tipo(46)]
 
-
+	# limpa os places e adiciona infos relevantes às observações
+	for row in rows:
+		for t_coleta in dicio['hora_coleta']:
+			row.append(t_coleta)
+	# row no formato:
+	# row + [col_year(47), col_mon(48), col_mday(49), col_hour(50), col_min(51),
+	#	col_sec(52), col_wday(53), col_yday(54), col_isds(55)
+	for row in rows:
+		print(len(row))
 	return rows
 
 
@@ -116,5 +145,5 @@ if __name__ == '__main__':
 
 
 
-	pp(pdata)
-	print(data[0].keys())
+	#pp(pdata)
+	#print(data[0].keys())
