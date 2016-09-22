6b60ab1bdd07e349f7aeb72a2d3fbc5c82d9c947
Matheus Facure <matheusfacure01@gmail.com>
2016-09-22 16:25:31 -0300

    Move o filtro de outliers para outro arquivo

--- tools/tranfs_train_test.py
+++ tools/tranfs_train_test.py
@@ -10,7 +10,7 @@ def test_regr(fit_regr, X_test, y_test, verbose = True):
 	s = ''
 	
 	pred = fit_regr.predict(X_test)
-	s += "\nTempo para testar:" + str(round(time()-t0, 3)) + "s"
+	s += "\n\nTempo para testar:" + str(round(time()-t0, 3)) + "s"
 
 	r2 = r2_score(y_test, pred)
 	s += '\nR^2 é: %.3f' % r2
@@ -30,7 +30,7 @@ def test_regr(fit_regr, X_test, y_test, verbose = True):
 
 def applyPCA(X_train, X_test, n_components, verbose = True):
 	if verbose:
-		print("Achando os top %d eigen-vectores" % n_components)
+		print("\n\nAchando os top %d eigen-vectores" % n_components)
 
 	t0 = time()
 	s = '\nDimensões da matriz antes de aplicar PCA filtrar:'
@@ -48,3 +48,23 @@ def applyPCA(X_train, X_test, n_components, verbose = True):
 		print(s)
 	return X_train, X_test 
 
+
+def remove_outliers(X_train, y_train, verbose = True):
+
+	top_outliers = y_train > np.mean(y_train) + 3 * np.std(y_train)
+	botom_outliers = y_train < np.mean(y_train) - 3 * np.std(y_train)
+	
+	tot_outliers = np.logical_not(np.logical_or(botom_outliers, top_outliers))
+	
+
+	s = '\n\nTemos %d outliers no topo' % sum(top_outliers)
+	s += '\nIsso equivale à %.3f' % (sum(top_outliers) / len(y_train)) + r'%'
+	s += '\nTemos %d outliers na base' % sum(botom_outliers)
+	s += 'Isso equivale à %.3f' % (sum(botom_outliers) / len(y_train)) + r'%'
+	if verbose:
+		print(s)
+
+	y_train = y_train[tot_outliers]
+	X_train = X_train[tot_outliers]
+
+	return X_train, y_train
