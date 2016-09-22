4a6005a33dcb965e32b0a5a23408f70ad04eb37b
Matheus Facure <matheusfacure01@gmail.com>
2016-09-22 15:04:45 -0300

    Move a tranformação PCA para outro arquivo

--- tools/tranfs_train_test.py
+++ tools/tranfs_train_test.py
@@ -2,6 +2,8 @@ import pandas as pd
 import numpy as np
 from time import time
 from sklearn.metrics import r2_score
+from sklearn.decomposition import PCA
+
 
 def test_regr(fit_regr, X_test, y_test, verbose = True):
 	t0 = time()	
@@ -25,3 +27,24 @@ def test_regr(fit_regr, X_test, y_test, verbose = True):
 		print(s)
 
 	return pred, abs_err, err_rel
+
+def applyPCA(X_train, X_test, n_components, verbose = True):
+	if verbose:
+		print("Achando os top %d eigen-vectores" % n_components)
+
+	t0 = time()
+	s = '\nDimensões da matriz antes de aplicar PCA filtrar:'
+	s += str(X_train.shape)
+
+	pca = PCA( n_components = n_components)
+	pca.fit(X_train)
+	X_train = pca.transform(X_train)
+	X_test = pca.transform(X_test)
+	s += '\nTop 5 componentes principais: '
+	s += str(pca.explained_variance_ratio_[:5]) 
+	s += "\nFeito em %0.3fs" % (time() - t0)
+	s += '\nDimensões da matriz após aplicas PCA:' + str(X_train.shape)
+	if verbose:
+		print(s)
+	return X_train, X_test 
+
